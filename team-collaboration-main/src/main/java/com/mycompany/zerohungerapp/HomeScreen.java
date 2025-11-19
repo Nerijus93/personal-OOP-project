5125e6772efc0426c485168d80aaab32eabac0f7
Nerijus93 <nerijus.kmitas@gmail.com>
2025-11-19 21:19:54 +0000

    Add files via upload

---
+++ team-collaboration-main/src/main/java/com/mycompany/zerohungerapp/HomeScreen.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.zerohungerapp;
+
+import java.awt.GridLayout;
+import javax.swing.*;
+
+/**
+ * @author User
+ * @author Tiyko - Ionut Ciobanu
+ * @author ITwork
+ */
+public class HomeScreen extends JFrame {
+
+    public HomeScreen() {
+        setTitle("Zero Hunger App");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JLabel pageTitleLabel = new JLabel("Welcome to The Zero Hunger App", SwingConstants.CENTER);
+
+        pageTitleLabel.setFont(pageTitleLabel.getFont().deriveFont(30f));
+
+        JPanel panel = new JPanel();
+
+        JButton donationButton = new JButton("Donations Management");
+        JButton memberButton = new JButton("Become a Member");
+        JButton restaurantButton = new JButton("Restaurant Manager");
+        
+        panel.setLayout(new GridLayout(0, 1));
+
+        // OPEN DonationScreen and SHOW it
+        donationButton.addActionListener(e -> {
+            DonationScreen ds = new DonationScreen();
+            ds.setVisible(true);
+            dispose();
+        });
+
+        add(pageTitleLabel, "North");
+
+        panel.add(donationButton);
+
+        panel.add(memberButton);
+
+        panel.add(restaurantButton);
+
+        add(panel);
+
+        setVisible(true);
+    }
+}
